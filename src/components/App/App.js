e5eae6fbbfe20c36ce265872a5d2216285902ea7
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-09-01 23:40:02 -0400

    ex2 initial font-end setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import GlobalStyles from "../GlobalStyles";
 
 const App = () => {
-  return <Router>TODO</Router>;
+  return (
+    <Router>
+      <h1>Hello World</h1>
+      <GlobalStyles />
+    </Router>
+  );
 };
 
 export default App;
