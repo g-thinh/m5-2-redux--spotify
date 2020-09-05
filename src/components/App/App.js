a81f50b70958606b0731b966e925adeacc06b9ae
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-09-04 20:40:30 -0400

    ex5 added routing and redirect to ArtistRoute

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import GlobalStyles from "../GlobalStyles";
+import ArtistRoute from "../artistRoute";
+
+const DEFAULT_ARTIST_ID = "1vCWHaC5f2uS3yhpwWbIA6";
 
 const App = () => {
   return (
     <Router>
-      <h1>Hello World</h1>
+      <Switch>
+        <Route exact={true} path="/">
+          <Redirect to={`/artist/${DEFAULT_ARTIST_ID}`} />
+        </Route>
+        <Route path="/artist/:id">
+          <ArtistRoute />
+        </Route>
+      </Switch>
       <GlobalStyles />
     </Router>
   );
---
+++ src/components/artistRoute.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const ArtistRoute = () => {
+  return <h1>This is the artist's page</h1>;
+};
+
+export default ArtistRoute;
