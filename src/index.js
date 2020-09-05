95eae89e258a52d17e32dec4db681b589d2f846f
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-09-04 20:21:53 -0400

    ex4 created and added redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import App from './components/App';
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import configureStore from "./store";
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
