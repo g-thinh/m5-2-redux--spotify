d54aff2e7a56506c8f57c26ff971f1c49349b2b6
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-09-04 20:59:49 -0400

    ex6 added actions to manage token and update store

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,12 +6,37 @@ import {
   Redirect,
 } from "react-router-dom";
 
+import { useDispatch } from "react-redux";
+
 import GlobalStyles from "../GlobalStyles";
 import ArtistRoute from "../artistRoute";
+import {
+  receiveAccessToken,
+  requestAccessToken,
+  receiveAccessTokenError,
+} from "../../actions";
 
 const DEFAULT_ARTIST_ID = "1vCWHaC5f2uS3yhpwWbIA6";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    console.log("[App.js] has mounted...");
+    dispatch(requestAccessToken());
+    const url = "/spotify_access_token";
+    try {
+      fetch(url)
+        .then((res) => res.json())
+        .then((json) => {
+          dispatch(receiveAccessToken(json.access_token));
+        });
+    } catch (error) {
+      console.log(error);
+      dispatch(receiveAccessTokenError());
+    }
+  }, []);
+
   return (
     <Router>
       <Switch>
--- src/components/artistRoute.js
+++ src/components/artistRoute.js
@@ -1,8 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
 
 const ArtistRoute = () => {
-  return <h1>This is the artist's page</h1>;
+  const accessToken = useSelector((state) => state.auth.token);
+
+  return <h1>This is the artist's access token</h1>;
 };
 
 export default ArtistRoute;
